fc108ff125f7adae138ca7432d1f93eb205280ab
Mark Halonen <halonen.mark@gmail.com>
2025-01-08 22:26:22 -0500

    asd

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
-    base: 'my-repo',
+    site: 'https://candid9.com'
+    // site: 'https://astronaut.github.io',
+    // base: 'my-repo',
 });
